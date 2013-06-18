a73aa1b5caa5c302f63a106b58ec433fb843c2b2
gpnuma <guillaume@centaurean.com>
2013-06-18 11:59:36 +0200

    License update

--- src/xor_hash_cipher.c
+++ src/xor_hash_cipher.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Guillaume Voirin (gvoirin@centaurean.com)
+ * Copyright (c) 2013, Guillaume Voirin
  * All rights reserved.
  *
  * This software is dual-licensed: you can redistribute it and/or modify
@@ -11,12 +11,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  * Alternatively, you can license this software under a commercial
- * license, as set out in licenses/commercial.txt.
- *
+ * license, as set out in licenses/commercial.txt. *
  * Centaurean SHARC
  * www.centaurean.com/sharc
  *
