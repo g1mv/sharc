f69888a931715d0660b1e116dd39f35a5f878049
gpnuma <guillaume@centaurean.com>
2013-09-03 20:17:45 +0200

    Big endian algorithms now fully functional for decoding as well

--- src/hash_decode.c
+++ src/hash_decode.c
@@ -95,13 +95,13 @@ SHARC_FORCE_INLINE SHARC_HASH_DECODE_STATE sharc_hash_decode_read_uncompressed_c
 }
 
 SHARC_FORCE_INLINE void sharc_hash_decode_compressed_chunk(const uint16_t *chunk, sharc_byte_buffer *restrict out, sharc_dictionary *restrict dictionary) {
-    *(uint32_t *) (out->pointer + out->position) = (&dictionary->entries[*chunk])->as_uint32_t;
+    *(uint32_t *) (out->pointer + out->position) = (&dictionary->entries[SHARC_LITTLE_ENDIAN_16(*chunk)])->as_uint32_t;
     out->position += sizeof(uint32_t);
 }
 
 SHARC_FORCE_INLINE void sharc_hash_decode_uncompressed_chunk(const uint32_t *chunk, sharc_byte_buffer *restrict out, sharc_dictionary *restrict dictionary, const uint32_t xorMask) {
     uint32_t hash;
-    SHARC_HASH_ALGORITHM(hash, *chunk, xorMask);
+    SHARC_HASH_ALGORITHM(hash, SHARC_LITTLE_ENDIAN_32(*chunk), xorMask);
     (&dictionary->entries[hash])->as_uint32_t = *chunk;
     *(uint32_t *) (out->pointer + out->position) = *chunk;
     out->position += sizeof(uint32_t);
